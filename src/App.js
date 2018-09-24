6873f27f6e4841522050403673ae3bd77e94c26b
Chris Hart <archipoetae@gmail.com>
2018-09-23 19:48:24 -0700

    Task #1 - chrish 2018.09.23  - Document the code in App.js to the best of your ability.
    Changelog:
    	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+// Config
 import React from 'react'
 import { Checkbox, Repeatable, Text, Textarea } from './components'
 import api from './mockApi'
 
+// React Component Class
 class App extends React.Component {
+  // Instantiate
   constructor(props) {
     super(props)
+    // Initialize State
     this.state = {
       data: {
         title: '',
@@ -16,35 +20,57 @@ class App extends React.Component {
       }
     }
 
+    // Bind Events
     this.handleChange = this.handleChange.bind(this)
     this.handleUpdate = this.handleUpdate.bind(this)
     this.Input = this.Input.bind(this)
   }
 
   /**
-   * 
+   * @func handleChange
+   * @desc Handle Form Element Change Event
+   * @param {object} delta - changed value data
    */
   handleChange(delta) {
+    // Update this.state.data
     this.setState(({ data }) => ({ data: { ...data, ...delta }}))
   }
 
   /**
-   * 
+   * @func handleUpdate
+   * @desc Handle Form Element Update Event
+   * @async
+   * @param {bool} publish - boolean toggle on saving data
    */
   async handleUpdate(publish = false) {
+    // Assign this.state.data to data
     const { data } = this.state
+
+    // Await the results (async)
     const results = await api.post({ ...data, publish })
     console.log('Content updated!')
+
+    // Return resolved or rejected promise
     return results
   }
 
   /**
-   * 
+   * @func Input
+   * @dsec Our Input Component
+   * @param {object} {} - anon object
+   *     @param {function} children - children as a function
+   *     @param {bool} iterable - element is repeatable
+   *     @param {string} label - Label for element
+   *     @param {string} id - id for element
    */
   Input({ children, iterable, label, id }) {
+    // handleChange Overload
+    // (so we can map change to element id iteratively)
     const handleChange = value => {
       this.handleChange({ [id]: value })
     }
+
+    // Assign state attr to value
     const value = this.state.data[id]
     let props = {}
 
@@ -72,6 +98,8 @@ class App extends React.Component {
         onChange: e => handleChange(e.target.value),
       }
     }
+
+    // Pass props to function-as-child-component
     return (
       <div className="Form-Group">
         <div className="Form-Label">{label}</div>
