768ed602a5e56a86dced4ffe26ea76b1f9c46747
Chris Hart <archipoetae@gmail.com>
2018-09-23 20:56:17 -0700

    Task #4 - chrish 2018.09.23
     - One of the components defined in ./components has a bug that will prevent it from behaving as expected.
       Identify and fix the bug:
    
       - The Checkbox component contained a bug wherein it's checked state
         did not affect the boolean value of this.state.data.upcoming.
       - Fixed in src/components/Checkbox.js
    
    Changelog:
    	modified:   src/components/Checkbox.js

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -1,3 +1,3 @@
 import React from 'react'
 
-export default props => <input type="checkbox" {...props} />
+export default props => <input type="checkbox" value={true} onChange={e => {e.target.value = (e.target.checked === true); props.onChange(e)}} defaultChecked={true} />
